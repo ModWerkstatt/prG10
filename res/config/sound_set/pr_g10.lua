51cd40f9f6ae0356f91f9bd1a8107f780a7eaee7
Florian Thiel <flth@flth.de>
2025-04-18 15:48:05 +0200

    Pfade korrigiert im Sound

--- res/config/sound_set/pr_g10.lua
+++ res/config/sound_set/pr_g10.lua
@@ -5,52 +5,52 @@ function data()
 local data = soundsetutil.makeSoundSet()
 
 soundsetutil.addTrackParam01
-(data, "vehicle/pr_g8/idle.wav", 25.0,
-		{ 
-		{ 0.0, 1.0 }, 
+(data, "vehicle/pr_g10/idle.wav", 25.0,
+		{
+		{ 0.0, 1.0 },
 		{ 0.05, 0.0 } },
-		{ 
-		{ .0, 1.0 }, 
-		{ 1.0, 1.0 } },
-		"speed01")	
-		
-soundsetutil.addTrackParam01
-(data, "vehicle/pr_g8/ventil.wav", 25.0,
-		{ 
-		{ 0.0, 0.0 }, 
-		{ 0.1, 2.0 }, 
-		{ 0.4, 0.0 } }, 
-		{ 
-		{ .0, 1.0 }, 
+		{
+		{ .0, 1.0 },
 		{ 1.0, 1.0 } },
 		"speed01")
-		
-		
+
 soundsetutil.addTrackParam01
-(data, "vehicle/pr_g8/fahrt.wav", 25.0,
-		{ 
+(data, "vehicle/pr_g10/ventil.wav", 25.0,
+		{
 		{ 0.0, 0.0 },
-		{ 0.05, 1.0 }, 
+		{ 0.1, 2.0 },
+		{ 0.4, 0.0 } },
+		{
+		{ .0, 1.0 },
+		{ 1.0, 1.0 } },
+		"speed01")
+
+
+soundsetutil.addTrackParam01
+(data, "vehicle/pr_g10/fahrt.wav", 25.0,
+		{
+		{ 0.0, 0.0 },
+		{ 0.05, 1.0 },
 		{ 1.0, 2.0 } },
-		{ 
-		{ 0.0, 0.7 }, 
+		{
+		{ 0.0, 0.7 },
 		{ 1.0, 1.4 } },
 		"speed01")
-	
+
 soundsetutil.addTrackParam01
-(data, "vehicle/pr_g8/roll.wav", 25.0,
+(data, "vehicle/pr_g10/roll.wav", 25.0,
 		{
 			{ 0.0, 0.0 },
 			{ 0.8, 0.1 },
 			{ 1.0, 1.0 } },
 		{
 			{ 0.0, 0.5 },
-			{ 1.0, 0.5 } }, 
+			{ 1.0, 0.5 } },
 		"speed01")
 
-soundsetutil.addEvent(data, "horn", { "vehicle/pr_g8/pfiff1.wav" }, 50.0)
-soundsetutil.addTrackSqueal(data, "vehicle/pr_g8/kurve.wav", 25.0)
-soundsetutil.addTrackBrake(data, "vehicle/pr_g8/bremsen.wav", 25.0, 1.0)
+soundsetutil.addEvent(data, "horn", { "vehicle/pr_g10/pfiff1.wav" }, 50.0)
+soundsetutil.addTrackSqueal(data, "vehicle/pr_g10/kurve.wav", 25.0)
+soundsetutil.addTrackBrake(data, "vehicle/pr_g10/bremsen.wav", 25.0, 1.0)
 
 
 return data
